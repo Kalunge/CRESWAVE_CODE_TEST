0aefc3a280177d48d7196b16e42bb47eef558947
Titus Muthomi <muthomititus+github@gmail.com>
2024-03-14 11:11:35 +0300

    Setup Docker, Db and other dependencies

---
+++ src/main/java/com/blog_api/blog_api/BlogApiApplication.java
@@ -0,0 +1,13 @@
+package com.blog_api.blog_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApiApplication.class, args);
+    }
+
+}
+//docker run -d --name mysql-container -e MYSQL_ROOT_PASSWORD=blog_db_password -e MYSQL_DATABASE=blog_db -p 3306:3306 mysql:latest
