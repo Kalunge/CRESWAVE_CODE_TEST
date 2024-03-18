1476c51bacaf2ec054321721aef1bf2d050327e8
Titus Muthomi <muthomititus+github@gmail.com>
2024-03-18 12:12:45 +0300

    Add method to retrieve a user by their username in repo

--- src/main/java/com/blog_api/blog_api/repository/UserRepository.java
+++ src/main/java/com/blog_api/blog_api/repository/UserRepository.java
@@ -3,8 +3,10 @@ package com.blog_api.blog_api.repository;
 import com.blog_api.blog_api.entity.Post;
 import com.blog_api.blog_api.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    UserDetails findByUsername(String username);
 }
