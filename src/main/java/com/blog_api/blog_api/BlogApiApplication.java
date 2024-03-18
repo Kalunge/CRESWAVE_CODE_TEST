de4fab696adc6954d038758481e064990e210cbc
Titus Muthomi <muthomititus+github@gmail.com>
2024-03-18 12:24:05 +0300

    Add TokenProvider Class

--- src/main/java/com/blog_api/blog_api/BlogApiApplication.java
+++ src/main/java/com/blog_api/blog_api/BlogApiApplication.java
@@ -5,8 +5,10 @@ import org.apache.log4j.PropertyConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@PropertySource("file:${user.dir}/.env")
 public class BlogApiApplication {
     public static void main(String[] args) {
         BasicConfigurator.configure();
