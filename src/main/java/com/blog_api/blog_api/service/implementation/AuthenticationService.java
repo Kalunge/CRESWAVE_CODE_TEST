b06ea2d1d84e33f7b4a65b353598e6736ce6e9fe
Titus Muthomi <muthomititus+github@gmail.com>
2024-03-19 11:07:01 +0300

    Add tests for comments

--- src/main/java/com/blog_api/blog_api/service/implementation/AuthenticationService.java
+++ src/main/java/com/blog_api/blog_api/service/implementation/AuthenticationService.java
@@ -1,15 +1,13 @@
 package com.blog_api.blog_api.service.implementation;
 
-import com.blog_api.blog_api.dto.JwtDto;
-import com.blog_api.blog_api.dto.SignInDto;
+
 import com.blog_api.blog_api.dto.SignUpDto;
 import com.blog_api.blog_api.entity.User;
 import com.blog_api.blog_api.exception.InvalidJwtException;
 import com.blog_api.blog_api.repository.UserRepository;
 import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -20,8 +18,11 @@ import org.springframework.stereotype.Service;
 @Log4j
 public class AuthenticationService implements UserDetailsService {
 
-    @Autowired
-    UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    public AuthenticationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) {
